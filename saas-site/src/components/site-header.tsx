948c7ac64e92791651267abc1f99b98376688aef
Gustavo Marcondes <marcondes.gustavo@gmail.com>
2025-12-26 13:54:05 +0000

    Remove dashboard link from header

--- saas-site/src/components/site-header.tsx
+++ saas-site/src/components/site-header.tsx
@@ -51,13 +51,6 @@ export default function SiteHeader() {
             >
               Sign up
             </Link>
-            
-            <Link
-              href="/dashboard"
-              className="hidden sm:inline-flex rounded-md border border-gray-300 px-3 py-2 text-sm hover:bg-gray-50"
-            >
-              Dashboard
-            </Link>
           </div>
         </div>
       </div>
