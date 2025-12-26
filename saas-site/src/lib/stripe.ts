3ea4b4188c830090646ec4485af0367fd96a7b36
Gustavo Marcondes <marcondes.gustavo@gmail.com>
2025-12-26 10:29:17 +0000

    Fix: Remove Stripe apiVersion parameter to use default

--- saas-site/src/lib/stripe.ts
+++ saas-site/src/lib/stripe.ts
@@ -3,8 +3,9 @@ import Stripe from 'stripe';
 const key = process.env.STRIPE_SECRET_KEY;
 
 // Stripe client (may be undefined if key not set)
+// Using default API version from Stripe package
 export const stripe = key
-  ? new Stripe(key, { apiVersion: '2025-12-15.clover' })
+  ? new Stripe(key)
   : (null as unknown as Stripe);
 
 export const SITE_URL =
